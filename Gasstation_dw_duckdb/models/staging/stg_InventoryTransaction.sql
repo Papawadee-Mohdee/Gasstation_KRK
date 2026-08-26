bda874415d5e65bd2b837691ad410d9f84543586
Jeanny P. <papawadee.m@kkumail.com>
2026-08-26 06:02:43 +0000

    test datawarehouse

--- Gasstation_dw_duckdb/models/staging/stg_InventoryTransaction.sql
+++ Gasstation_dw_duckdb/models/staging/stg_InventoryTransaction.sql
@@ -1,8 +1,8 @@
 select
     transactionid::int      as transaction_id,
-    tankid::int                as tank_id,
-    quantityin::decimal          as quantity_in,
-    quantityout::decimal           as quantity_out,
-    remainingquantity::decimal       as remaining_quantity,
-    transactiondate::timestamp         as transaction_date
+    tankid::int              as tank_id,
+    quantityin::decimal        as quantity_in,
+    quantityout::decimal         as quantity_out,
+    remainingquantity::decimal     as remaining_quantity,
+    strptime(transactiondate, '%d/%m/%Y %H:%M')::timestamp as transaction_date
 from {{ source('gas_station_raw', 'inventorytransaction') }}
